3334dbd48cefabb7945d4292b89da35c1a095c07
feiwang <wangfei@chunyu.me>
2015-08-17 18:54:04 +0800

    添加了iputils的实现

---
+++ src/ip_query/ip_query_v1.go
@@ -0,0 +1,182 @@
+package ip_query
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/qiniu/iconv"
+	"io/ioutil"
+	"os"
+)
+
+type IpInfoServiceV1 struct {
+	IpIndexes []*ipIndex
+
+	cd   *iconv.Iconv
+	fbuf []byte
+}
+
+func (p *IpInfoServiceV1) Ip2Address(ip string) (country string, city string) {
+
+	intIP := inet_aton(ip)
+
+	start := 0
+	end := len(p.IpIndexes) - 1
+	var mid int
+
+	for start <= end {
+		mid = (start + end) / 2
+		if intIP < p.IpIndexes[mid].Ip {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+
+	// 最终的结果：
+	// IP[end] <= intIP
+	index := p.IpIndexes[end]
+	return p.getAddr(index.Offset + 4)
+}
+
+func (p *IpInfoServiceV1) LoadData(filename string) error {
+
+	fid, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	p.fbuf, err = ioutil.ReadAll(fid)
+	fid.Close()
+	if err != nil {
+		return err
+	}
+
+	var indexStart uint32
+	var indexEnd uint32
+
+	// 读取索引的起止位置
+	indexStart = binary.LittleEndian.Uint32(p.fbuf[0:4])
+	indexEnd = binary.LittleEndian.Uint32(p.fbuf[4:8])
+
+	fmt.Printf("Index Start: %d, End: %d\n", indexStart, indexEnd)
+
+	itemNum := (indexEnd - indexStart) / 7
+	p.IpIndexes = make([]*ipIndex, itemNum, itemNum)
+	//	p.IpRecords = make([]*IpRecord, itemNum, itemNum)
+
+	var i uint32
+	offset := indexStart
+	for i = 0; i < itemNum; i++ {
+		offset += 7
+		index := &ipIndex{}
+		index.Ip = binary.LittleEndian.Uint32(p.fbuf[offset:(offset + 4)])
+		index.Offset = byte3ToUint32(p.fbuf[(offset + 4):(offset + 7)])
+
+		p.IpIndexes[i] = index
+	}
+
+	fmt.Println("Index Decoding Succeed")
+
+	// https://github.com/qiniu/iconv
+	cd, err := iconv.Open("utf-8", "gbk") // 从GBK转换成为utf8
+	if err != nil {
+		fmt.Println("iconv.Open failed!")
+		return err
+	}
+	p.cd = &cd
+
+	fmt.Printf("Data Load Complete: %d items: ", itemNum)
+	return nil
+
+}
+
+func (p *IpInfoServiceV1) Close() {
+	if p.cd != nil {
+		p.cd.Close()
+		p.cd = nil
+	}
+}
+
+// offset: 为country, city信息所在位置的offset, 跳过了之前的IP
+func (p *IpInfoServiceV1) getAddr(offset uint32) (country string, city string) {
+
+	var idx int
+	order := p.fbuf[offset]
+
+	if order == 1 {
+		offset += 1
+		newOffset := byte3ToUint32(p.fbuf[offset:len(p.fbuf)])
+		return p.getAddr(newOffset)
+
+	} else if order == 2 {
+		offset += 1
+		country = p.getAreaAddr(byte3ToUint32(p.fbuf[offset : offset+3]))
+		offset += 3
+		city = p.getAreaAddr(offset)
+
+	} else {
+		country, idx = p.getString(p.fbuf[offset:len(p.fbuf)])
+		city, _ = p.getString(p.fbuf[offset+uint32(idx) : len(p.fbuf)])
+	}
+	return
+}
+
+func (p *IpInfoServiceV1) getAreaAddr(offset uint32) string {
+	order := p.fbuf[offset]
+	if order == 1 || order == 2 {
+		offset += 1
+		offset = byte3ToUint32(p.fbuf[offset : offset+3])
+		return p.getAreaAddr(offset)
+	} else {
+		result, _ := p.getString(p.fbuf[offset:len(p.fbuf)])
+		return result
+	}
+}
+
+func (p *IpInfoServiceV1) getString(slice []byte) (term string, idx int) {
+
+	for idx < len(slice) {
+		if slice[idx] == 0 {
+			break
+		} else {
+			idx += 1
+		}
+	}
+
+	str := slice[0:idx]
+	termBuff := make([]byte, len(str)*3)
+	out, _, _ := p.cd.Conv(str, termBuff)
+	return string(out), idx + 1
+}
+
+//func byte3ToUint32(offset []byte) uint32 {
+//	return uint32(offset[0]) + (uint32(offset[1]) << 8) + (uint32(offset[2]) << 16)
+//}
+
+//func inet_ntoa(ipnr int64) net.IP {
+//	var bytes [4]byte
+//	bytes[0] = byte(ipnr & 0xFF)
+//	bytes[1] = byte((ipnr >> 8) & 0xFF)
+//	bytes[2] = byte((ipnr >> 16) & 0xFF)
+//	bytes[3] = byte((ipnr >> 24) & 0xFF)
+
+//	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0])
+//}
+
+//func inet_aton(ipnr string) uint32 {
+//	bits := strings.Split(ipnr, ".")
+
+//	b0, _ := strconv.Atoi(bits[0])
+//	b1, _ := strconv.Atoi(bits[1])
+//	b2, _ := strconv.Atoi(bits[2])
+//	b3, _ := strconv.Atoi(bits[3])
+
+//	var sum uint32
+
+//	sum += uint32(b0) << 24
+//	sum += uint32(b1) << 16
+//	sum += uint32(b2) << 8
+//	sum += uint32(b3)
+
+//	return sum
+//}
--- src/ip_query_service.go
+++ src/ip_query_service.go
@@ -6,6 +6,11 @@ import (
 	"time"
 )
 
+//  		 	    IpInfoService   vs. IpInfoServiceV1
+//    内存: 		     67M					28M
+//    启动时间:     5.5206s			   0.3227s
+// 单次处理时间:     0.0367s			   0.1508s * 10-4
+//  对应的Python实现: 22M内存             0.30236s
 func main() {
 	filename := "/Users/feiwang/gowork/src/git.chunyu.me/infra/ip_utils/src/ip_query/qqwry.dat"
 
@@ -20,11 +25,39 @@ func main() {
 		panic(fmt.Sprintf("File Open Error: %s", filename))
 	}
 
-	country, city := service.Ip2Address("60.29.255.197")
+	t1 = time.Now()
+	for i := 0; i < 10000; i++ {
+		_, _ = service.Ip2Address("60.29.255.197")
+	}
+	t2 = time.Now()
+	fmt.Printf("Elapsed: %.4fms\n", float64(t2.UnixNano()-t1.UnixNano())*10e-10)
 
+	country, city := service.Ip2Address("60.29.255.197")
 	fmt.Println("Country: ", country, ", City: ", city)
 
 	//city, _detail = ip_utils.get_default_ip_info().getIPAddr('60.29.255.197')
 	//        self.assertEqual(u'天津市', ensure_unicode(city))
+
+	serviceV1 := ip_query.IpInfoServiceV1{}
+	t1 = time.Now()
+	err = serviceV1.LoadData(filename)
+	t2 = time.Now()
+
+	// 10e-9 * 10e-4 * 1e3
+	fmt.Printf("Elapsed: %.4fs\n", float64(t2.UnixNano()-t1.UnixNano())*10e-9)
+
+	if err != nil {
+		panic(fmt.Sprintf("File Open Error: %s", filename))
+	}
+
+	t1 = time.Now()
+	for i := 0; i < 10000; i++ {
+		_, _ = serviceV1.Ip2Address("60.29.255.197")
+	}
+	t2 = time.Now()
+	fmt.Printf("Elapsed: %.4fms\n", float64(t2.UnixNano()-t1.UnixNano())*10e-10)
+
+	country, city = serviceV1.Ip2Address("60.29.255.197")
+	fmt.Println("Country: ", country, ", City: ", city)
 
 }
